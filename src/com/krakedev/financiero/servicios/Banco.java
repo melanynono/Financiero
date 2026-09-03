57fd81c3ee36094b099015f2376bea1918bcc797
MelanyNono <melanynono123@gmail.com>
2026-09-02 22:32:02 -0500

    Crear paquete servicios, metodo crearCuenta

---
+++ src/com/krakedev/financiero/servicios/Banco.java
@@ -0,0 +1,30 @@
+package com.krakedev.financiero.servicios;
+
+import com.krakedev.financiero.entidades.Cliente;
+import com.krakedev.financiero.entidades.Cuenta;
+
+public class Banco {
+
+	private int ultimoCodigo = 1000;
+	
+	public Banco() {
+		
+	}
+
+	public int getUltimoCodigo() {
+		return ultimoCodigo;
+	}
+
+	public void setUltimoCodigo(int ultimoCodigo) {
+		this.ultimoCodigo = ultimoCodigo;
+	}
+	
+	public Cuenta crearCuenta(Cliente cliente) {
+		
+		String codigoStr = ultimoCodigo + "";
+		ultimoCodigo++;
+		Cuenta cuenta = new Cuenta(codigoStr);
+		cuenta.setPropietario(cliente);
+		return cuenta;
+	}
+}
