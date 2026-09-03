e29285c0db558994e91e68b670e23b7172cfc6dc
MelanyNono <melanynono123@gmail.com>
2026-09-02 22:08:53 -0500

    Crear clase Cuenta y Cliente con metodos

---
+++ src/com/krakedev/financiero/entidades/Cliente.java
@@ -0,0 +1,50 @@
+package com.krakedev.financiero.entidades;
+
+public class Cliente {
+	
+	private String cedula;
+	private String nombre;
+	private String apellido;
+	
+	public Cliente() {
+		
+	}
+	
+	public Cliente(String cedula, String nombre, String apellido) {
+		this.cedula = cedula;
+		this.nombre = nombre;
+		this.apellido = apellido;
+	}
+
+	public String getCedula() {
+		return cedula;
+	}
+
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	
+	
+	public void imprimir() {
+		System.out.println("Cedula: "+ cedula);
+		System.out.println("Nombre: "+ nombre);
+		System.out.println("Apellido: "+ apellido);
+	}
+
+}
---
+++ src/com/krakedev/financiero/entidades/Cuenta.java
@@ -0,0 +1,68 @@
+package com.krakedev.financiero.entidades;
+
+public class Cuenta {
+	
+	private String id;
+	private double saldoActual;
+	private String tipo;
+	private Cliente propietario;
+	
+	
+	public Cuenta(String id) {
+		this.id = id;
+		this.saldoActual = 0;
+		this.tipo ="A";
+	}
+
+
+	public String getId() {
+		return id;
+	}
+
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+
+	public double getSaldoActual() {
+		return saldoActual;
+	}
+
+
+	public void setSaldoActual(double saldoActual) {
+		this.saldoActual = saldoActual;
+	}
+
+
+	public String getTipo() {
+		return tipo;
+	}
+
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
+	
+	public Cliente getPropietario() {
+		return propietario;
+	}
+
+
+	public void setPropietario(Cliente propietario) {
+		this.propietario = propietario;
+	}
+
+
+	public void imprimir() {
+		System.out.println("ID: "+ id);
+		System.out.println("Saldo Actual: "+ saldoActual);
+		System.out.println("Tipo: "+ tipo);
+		System.out.println("Cédula cliente: " + propietario.getCedula());
+		System.out.println("Nombre cliente: " + propietario.getNombre());
+		System.out.println("Aoellido cliente: " + propietario.getApellido());
+	}
+	
+
+}
